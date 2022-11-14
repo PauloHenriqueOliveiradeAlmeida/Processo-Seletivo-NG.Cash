c72bfb81ba7a45df8b273e166e862413d6da11cf
PauloHenriqueOliveiradeAlmeida <paulo333henrique00@gmail.com>
2022-11-14 19:27:34 -0300

    iniciando o projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import SessionForm from "../components/SessionForm/SessionForm"
+
+export default function Home() {
+  return (
+    <div>
+      <SessionForm loginOrRegistration="Cadastro"/>
+    </div>
+  )
+}
